ebe4eb4f1049061cbd7f495ceb39300e7a14b37d
FahidHabib <fahid.habib84@gmail.com>
2021-01-12 14:27:05 +0500

    Update AppReducer.js

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,7 +1,4 @@
-import React from 'react';
-
-
-export default  (state,action)=>{
+export default  (state, action)=>{
     switch(action.type){
         case 'ADD_TRANSACTION':
             return{
@@ -18,3 +15,4 @@ export default  (state,action)=>{
             return state;
     }
 }
+
